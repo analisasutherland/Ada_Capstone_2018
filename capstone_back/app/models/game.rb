a0d496b1b0160b871d3127a4e0c98dde2d2dcaa9
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-07-12 10:42:48 -0700

    Clean up changes, removed binding.prys

--- capstone_back/app/models/game.rb
+++ capstone_back/app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
+  # TODO: add back in the associations 
   # has_many :reviews
   # has_many :recommendations
   validates :game_title, presence: true
--- capstone_back/db/seeds.rb
+++ capstone_back/db/seeds.rb
@@ -3,32 +3,23 @@ require 'pry'
 
 review_data = WombatScraper.new.crawl
 
-# puts review_data
-
 review_data['the_articles'].each do |review|
   # if article is empty or it is nil, jump to the next
   next if review['article'].nil? || review['article'].empty?
 
   tentative_title = review['article'][0]['titles']
 
-  next if tentative_title.length == 0
+  next if tentative_title.empty? || tentative_title.nil?
+
+# TODO: Need to simplfy way that title is set, consider using .first for an array to get first occuring title else if the first element is not the one we want (why??), we will use the long method
 
   if tentative_title.nil? || tentative_title.length.nil?
     title = 'untitled'
   else
-    begin
-      title = tentative_title.group_by(&:itself).values.max_by(&:size).first
-    # rescue
-    #   binding.pry
-    end
+    title = tentative_title.group_by(&:itself).values.max_by(&:size).first
   end
 
-  my_game = Game.create!(game_title: title) if Game.find_by(game_title: title).nil?
-
-  # puts "#{title} is valid: #{my_game.valid?}"
-  # if !my_game.valid?
-  #   puts "my_game.errors"
-  # end
+  Game.create!(game_title: title) if Game.find_by(game_title: title).nil?
 
   # The content is broken up into separate index based on paragraphs. Joining them together at a line break
   content = review['article'][0]['content']
