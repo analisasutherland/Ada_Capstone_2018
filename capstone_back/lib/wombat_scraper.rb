4c3ff707918ac32071180b19d9fe32b21401a483
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-07-20 15:45:25 -0700

    Seeded the database with more games.

--- capstone_back/lib/wombat_scraper.rb
+++ capstone_back/lib/wombat_scraper.rb
@@ -4,14 +4,13 @@ require 'pry'
 class WombatScraper
   include Wombat::Crawler
   base_url 'https://www.polygon.com'
+
+  # NOTE: Did a manual load of games by changing url before seeding and reseeding multiple times
+
   path '/reviews/archives'
-  # TODO: Need a loop that loops through all the paginations of articles index pages
-  # article_indexs 'css=a.c-pagination__next c-pagination__link', :follow do
-
-  # TODO: The images
-
+  # article_index 'css=a.c-pagination__next.c-pagination__link', :follow do
   # This loop loops through all articles on a given index page ie page 1
-  the_articles 'css=div.c-entry-box--compact__body a', :follow do
+  the_articles 'css=div.c-entry-box--compact__body h2 a', :follow do
     # Loops through a given article and pulls out all <p> tag content
     article 'css=div.c-entry-content', :iterator do |article|
       article.titles 'css=em', :list
@@ -20,10 +19,10 @@ class WombatScraper
       # binding.pry
     end
   end
+  # end
 end
-
-new_scrape = WombatScraper.new.crawl
-puts new_scrape.to_a
+# new_scrape = WombatScraper.new.crawl
+# p new_scrape.to_a
 
 
 # <a class="c-pagination__next c-pagination__link p-button" href="/reviews/archives/2">Next</a>
